4efb05e11908e21b2055b9e6a1a8f0ccd750b8d2
Justin B Kay <jkay@nwrecc.org>
2014-04-14 14:05:32 -0600

    session controller logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.where(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: "Login successful!"
+    else
+      flash.now.alert = "Authentication failed"
+      render :new
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: "Logged out!"
   end
 end
