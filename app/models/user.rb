418e687a2b16e67598648cb6abd5599e4ac9f05c
Justin B Kay <jkay@nwrecc.org>
2014-04-14 15:48:36 -0600

    some validations for the users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+
 end
