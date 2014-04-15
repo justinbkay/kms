ec7470e69af34257801ed501d2acb861a48ca3dc
Justin B Kay <jkay@nwrecc.org>
2014-04-15 10:20:57 -0600

    Loading the separate forms for major and minor

--- app/controllers/office_direct_referrals_controller.rb
+++ app/controllers/office_direct_referrals_controller.rb
@@ -2,9 +2,21 @@ class OfficeDirectReferralsController < ApplicationController
   def new
   end
 
+  def create_major
+
+  end
+
   def index
   end
 
   def edit
+  end
+
+  def major_form
+    render '_major_form'
+  end
+
+  def minor_form
+    render '_minor_form'
   end
 end
