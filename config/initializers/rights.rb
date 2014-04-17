198cae77ea91bc4e75c795ada0c61d221b07c379
Justin <justinbkay@gmail.com>
2014-04-16 19:57:29 -0600

    adding the rights constant

--- config/initializers/rights.rb
+++ config/initializers/rights.rb
@@ -1,6 +1,14 @@
 RIGHTS = {}
 RIGHTS[:staff] = [
-                  ['sessions', 'destroy']
+                  ['sessions', 'destroy'],
+                  ['office_direct_referrals', 'new'],
+                  ['office_direct_referrals', 'create_major'],
+                  ['office_direct_referrals', 'index'],
+                  ['office_direct_referrals', 'edit'],
+                  ['office_direct_referrals', 'update'],
+                  ['office_direct_referrals', 'major_form'],
+                  ['office_direct_referrals', 'minor_form'],
+                  ['office_direct_referrals', 'student_search']
                  ]
-RIGHTS[:administrator] = [[]]
+RIGHTS[:administrator] = [[]] + RIGHTS[:staff]
 RIGHTS[:superuser] = [[]] + RIGHTS[:staff]
