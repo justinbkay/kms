b2438495b6378582f289697168a30b91da65d037
Justin B Kay <jkay@nwrecc.org>
2014-04-18 10:10:29 -0600

    adding validation on referrals

--- app/models/office_direct_referral.rb
+++ app/models/office_direct_referral.rb
@@ -1,4 +1,9 @@
 class OfficeDirectReferral < ActiveRecord::Base
   belongs_to :student
   belongs_to :user
+
+  validates :student_id, :presence => true
+  validates :block_id, :presence => true
+  validates :user_id, :presence => true
+  validates :incident_date, :presence => true
 end
