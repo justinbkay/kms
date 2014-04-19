8e8dac3faab6237b06d1d35087b4a1d7b4d2ad24
Justin <justinbkay@gmail.com>
2014-04-19 15:17:25 -0600

    adding a separate controller for minor offenses

---
+++ app/controllers/minor_direct_referral_controller.rb
@@ -0,0 +1,10 @@
+class MinorDirectReferralController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
---
+++ test/helpers/minor_direct_referral_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class MinorDirectReferralHelperTest < ActionView::TestCase
+end
