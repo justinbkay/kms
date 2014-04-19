dbbd8d50607db96a953367d235e55e6af54baeed
Justin <justinbkay@gmail.com>
2014-04-19 15:19:08 -0600

    making incident date separate in the form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'minor_direct_referral/index'
+
+  get 'minor_direct_referral/new'
+
+  get 'minor_direct_referral/edit'
+
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
@@ -7,6 +13,7 @@ Rails.application.routes.draw do
 
   get 'office_direct_referrals/minor_form'
   get 'office_direct_referrals/major_form'
+  match 'office_direct_referrals/major_form' => 'office_direct_referrals#create_major_form', :via => :post, :as => 'create_major_form'
 
   resources :users
   resources :sessions
