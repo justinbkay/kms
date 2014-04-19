a878073b4dcfeceb60449394c6568eacbc1073f9
Justin <justinbkay@gmail.com>
2014-04-19 15:20:02 -0600

    fixing rights for new controller

--- config/initializers/rights.rb
+++ config/initializers/rights.rb
@@ -2,12 +2,10 @@ RIGHTS = {}
 RIGHTS[:staff] = [
                   ['sessions', 'destroy'],
                   ['office_direct_referrals', 'new'],
-                  ['office_direct_referrals', 'create_major'],
+                  ['office_direct_referrals', 'create'],
                   ['office_direct_referrals', 'index'],
                   ['office_direct_referrals', 'edit'],
                   ['office_direct_referrals', 'update'],
-                  ['office_direct_referrals', 'major_form'],
-                  ['office_direct_referrals', 'minor_form'],
                   ['office_direct_referrals', 'student_search']
                  ]
 RIGHTS[:administrator] = [[]] + RIGHTS[:staff]
