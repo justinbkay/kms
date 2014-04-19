6fca4d7a43c1e093871e6d874514aadb6cb7ecfc
Justin <justinbkay@gmail.com>
2014-04-19 15:22:18 -0600

    making new and create there own thing

--- app/controllers/office_direct_referrals_controller.rb
+++ app/controllers/office_direct_referrals_controller.rb
@@ -2,28 +2,33 @@ class OfficeDirectReferralsController < ApplicationController
   before_filter :security
 
   def new
+    @office_direct_referral = OfficeDirectReferral.new(user_id: current_user.id)
   end
 
-  def create_major
+  def create
+    @office_direct_referral = OfficeDirectReferral.new(referral_params)
+    zone = "Mountain Time (US & Canada)"
 
+    @office_direct_referral.incident_date = ActiveSupport::TimeZone[zone].parse("#{params[:incident_date]} #{params[:incident_time]}:00")
+
+    if @office_direct_referral.save
+      redirect_to root_url
+    else
+      render :new
+    end
   end
 
   def index
+    @office_direct_referrals = OfficeDirectReferral.all
   end
 
   def edit
   end
 
-  def major_form
-    @office_direct_referral = OfficeDirectReferral.new(user_id: current_user.id)
-    render '_major_form'
+  private
+
+  def referral_params
+    params.require(:office_direct_referral).permit!
   end
 
-  def minor_form
-    render '_minor_form'
-  end
-
-  def student_search
-    render json: Student.where("lower(first_name) like ? AND active='t'", "%#{params[:term].downcase}%").map { |s| [id: s.id, grade: s.grade, label: "#{s.first_name} #{s.last_name}  (#{s.student_number})"]}.flatten
-  end
 end
