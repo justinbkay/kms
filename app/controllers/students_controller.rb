a94a2055d3380c42850a807517d9b2c75344416b
Justin B Kay <jkay@nwrecc.org>
2014-04-21 16:40:59 -0600

    adding methods for managing students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,17 +1,33 @@
 class StudentsController < ApplicationController
   def new
+    @student = Student.new
   end
 
   def index
   end
 
   def create
+    @student = Student.new(student_params)
+
+    if @student.save
+      redirect_to root_url
+    else
+      render :new
+    end
   end
 
   def edit
+    @student = Student.find(params[:id])
   end
 
   def update
+    @student = Student.find(params[:id])
+
+    if @student.update_attributes(student_params)
+      redirect_to root_url
+    else
+      render :edit
+    end
   end
 
   def search
@@ -25,4 +41,11 @@ class StudentsController < ApplicationController
     end
     @students = Student.search_by_name(names)
   end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:first_name, :last_name, :student_number, :grade, :year, :active)
+  end
+
 end
